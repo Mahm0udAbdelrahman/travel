46cdafbf0ecfe9bdb0979ca635a730f20d730cc7
Mahm0udAbdelrahman <mahmudabdelrahman0@gmail.com>
2026-02-01 23:52:21 +0200

    favoritable_id

--- app/Http/Resources/FavoriteResource.php
+++ app/Http/Resources/FavoriteResource.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
@@ -15,12 +14,14 @@ class FavoriteResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'   => $this->id,
-            'favoritable_name' => $this->favoritable->name[app()->getLocale()],
-            'favoritable_image' => $this->favoritable->image,
-            'favoritable_price' => $this->favoritable->price ?? 0,
+            'id'                      => $this->id,
+            'favoritable_id'          => $this->favoritable->id,
+            'favoritable_type'        => $this->favoritable_type,
+            'favoritable_name'        => $this->favoritable->name[app()->getLocale()],
+            'favoritable_image'       => $this->favoritable->image,
+            'favoritable_price'       => $this->favoritable->price ?? 0,
             'favoritable_description' => $this->favoritable->description[app()->getLocale()],
-            'is_favorite' => $this->is_favorite,
+            'is_favorite'             => $this->is_favorite,
 
         ];
     }
