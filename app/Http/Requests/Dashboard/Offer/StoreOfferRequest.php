361747fc60b87c63e7e83af521e48ad226b5c6c5
Mahm0udAbdelrahman <mahmudabdelrahman0@gmail.com>
2026-02-23 03:25:20 +0200

    offer

--- app/Http/Requests/Dashboard/Offer/StoreOfferRequest.php
+++ app/Http/Requests/Dashboard/Offer/StoreOfferRequest.php
@@ -48,11 +48,9 @@ class StoreOfferRequest extends FormRequest
             'is_active'       => ['required', 'boolean'],
             'excursion_ids'   => ['required', 'array'],
             'excursion_ids.*' => ['integer', 'exists:excursions,id'],
-            // 'days'            => ['nullable', 'array'],
-            // 'days.*'          => ['array'],
-            // 'days.*.*'        => ['integer', 'exists:excursion_days,id'],
             'times'           => ['nullable', 'array'],
-            'times.*'         => ['integer', 'exists:excursion_times,id'],
+            'times.*'         => ['array'], // كل رحلة فيها مصفوفة أوقات
+            'times.*.*'       => ['integer', 'exists:excursion_times,id'],
         ];
     }
 
